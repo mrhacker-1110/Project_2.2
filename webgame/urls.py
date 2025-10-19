f7349cda3933573423c206713268ad760ca37899
Kudaibergen Myrzabekov <myrzabekov_k@iuca.kg>
2025-10-19 16:02:30 +0600

    Add URL routes for game pages

--- webgame/urls.py
+++ webgame/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from tictactoe import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('new/', views.new_game, name='new_game'),
+    path('play/<int:game_id>/', views.play_game, name='play_game'),
 ]
